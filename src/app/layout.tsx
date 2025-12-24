cecf8679f9f1d1b861937f01712d58d2949159f6
Ali-Fayzullaev <alifayzullaev2020@gmail.com>
2025-12-24 15:13:46 +0500

    feat: Delete old verification files and add new ones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,20 @@ export default function RootLayout({
         <link rel="alternate" hrefLang="ru-kz" href="https://www.raycon.kz/" />
         <link rel="alternate" hrefLang="kk-kz" href="https://www.raycon.kz/" />
         
-        {/* Google Analytics */}
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T77P85GS');
+            `,
+          }}
+        />
+        
+        {/* Google Analytics */}}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z6QYLQ4C27"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -81,6 +94,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T77P85GS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
