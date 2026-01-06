261a6b3b9f99ec7a45a2df12bc98f3fe97f0de1c
Ali-Fayzullaev <alifayzullaev2020@gmail.com>
2026-01-06 16:51:52 +0500

    feat: Add a Yandex.Metrika tag for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,6 +89,22 @@ export default function RootLayout({
             `,
           }}
         />
+        
+        {/* Yandex.Metrika counter */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105992559', 'ym');
+
+              ym(105992559, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -101,6 +117,17 @@ export default function RootLayout({
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
+        </noscript>
+        
+        {/* Yandex.Metrika (noscript) */}
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/105992559" 
+              style={{ position: 'absolute', left: '-9999px' }} 
+              alt="" 
+            />
+          </div>
         </noscript>
         
         <script
